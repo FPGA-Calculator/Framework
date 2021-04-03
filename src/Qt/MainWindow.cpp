e7e08bd1b025012a8868a8e1880a5064de03a09e
Goran Devic <gdevic@yahoo.com>
2021-04-03 13:08:41 -0500

    Qt app displays (blank) LCD

--- src/Qt/MainWindow.cpp
+++ src/Qt/MainWindow.cpp
@@ -1,11 +1,17 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
+#include <QLabel>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QLabel *l = ui->centralwidget->findChild<QLabel *>("lcd");
+    QPixmap pic(":/vrEmuLcd.gif");
+    l->setPixmap(pic);
 }
 
 MainWindow::~MainWindow()
