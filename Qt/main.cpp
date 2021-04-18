c183bc92f56b15facac0b4c4a9cfcde89b929ff2
Goran Devic <gdevic@yahoo.com>
2021-04-17 22:21:03 -0500

    Qt app, in debug mode copy resource instead of using qrc

--- Qt/main.cpp
+++ Qt/main.cpp
@@ -7,9 +7,17 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+#ifdef QT_DEBUG
+    // In debug mode, use the resources from their source location since the qrc file
+    // is not being refreshed (unless fully rebuilding). This saves time, but will
+    // not work in WebAssembly. We don't debug in that environment, anyways.
+    QFile::remove("lcd_cmd.txt"); // copy will not force...
+    QFile::copy("../verilog/lcd_cmd.txt", "lcd_cmd.txt");
+#else
     // Copy resource file into the file system on the target platform
-    // so it can be read by the async_rom module (using the $readmemb construct)
+    // so it can be read by the async_rom module (using the $readmemb function)
     QFile::copy(":/lcd_cmd", "lcd_cmd.txt");
+#endif // QT_DEBUG
 
     ClassSim sim(argc, argv);
 
